d11bd40d61e64e6fd33897e2ce609f852a2991f0
MOHIT-848 <singhsaabh1548@gmail.com>
2025-05-06 12:57:23 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,21 @@
+### BASIC CALCULATOR ####
+a= int(input("enter first number :"))
+b= int(input("enter second number :"))
+c= a+b
+print("ADDITION =",c)
+if a>b:
+    d= a-b
+    print("DIFFERENCE=",d)
+else:
+    dd= b-a
+    print("DIFFERENCE=",dd)
+m= a*b
+print("PRODUCT=",m)
+if a>b:
+    r=a/b
+    print("DIVSION=",r)
+else:
+    rr=b/    print("DIVSION=",r)
+    print("DIVSION=",rr)
+#####program end #######
+    
